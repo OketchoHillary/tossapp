b8c8ed51336dd7a46d7001df9bf80a646717bbc6
KatongoleHerbert <khakatongole@gmail.com>
2019-10-02 12:58:44 +0300

    test1

--- lotto_api/management/commands/d_sched.py
+++ lotto_api/management/commands/d_sched.py
@@ -11,8 +11,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # schedule.every(10).minutes.do(create_daily_lotto)
         # schedule.every(11).minutes.do(daily_draw)
-        schedule.every().day.at("16:57").do(create_daily_lotto)
-        schedule.every().day.at("17:03").do(daily_draw)
+        schedule.every().day.at("13:05").do(create_daily_lotto)
+        schedule.every().day.at("13:15").do(daily_draw)
 
         while True:
             schedule.run_pending()
