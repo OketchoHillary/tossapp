ecf12b105c8eac03948407ea585fd9f5d8148da8
KatongoleHerbert <khakatongole@gmail.com>
2019-10-02 13:23:43 +0300

    pagination

--- tauth/settings.py
+++ tauth/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 1
+        'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 5
 }
 COUNTRIES_FIRST_REPEAT = True
 
